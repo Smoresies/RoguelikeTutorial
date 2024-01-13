c6fa771cf23e8eb7cc80ef08f205385a16440beb
Smoresies <gavin.britton@gmail.com>
2024-01-12 19:22:38 -0800

    Ch3 Midway, Dungeon Generation started, tiles create 2 rooms.

--- src/Game/map.gd
+++ src/Game/map.gd
@@ -1,16 +1,15 @@
 class_name Map
 extends Node2D
 
-# width/height. Allows us to change in editor.
-@export var map_width: int = 80
-@export var map_height: int = 45
+# Create/Find Dungeon Generator object
+@onready var dungeon_generator: DungeonGenerator = $DungeonGenerator
 
 # Holds all our MapData
 var map_data: MapData
 
 # Creates map_data, then calls _place_tiles()
 func _ready():
-	map_data = MapData.new(map_width, map_height)
+	map_data = dungeon_generator.generate_dungeon()
 	_place_tiles()
 
 # Attaches all the tiles in map_data to be children of Map node
---
+++ src/Map/dungeon_generator.gd
@@ -0,0 +1,39 @@
+class_name DungeonGenerator
+extends Node
+
+# Inspector Variables
+@export_category("Map Dimensions")
+@export var map_width: int = 80
+@export var map_height: int = 45
+
+# Create some random number
+var _rng := RandomNumberGenerator.new()
+
+# On ready, just randomize
+func _ready() -> void:
+	_rng.randomize()
+
+# Carving out the layout of the dungeon, given x/y turn to floor.
+func _carve_tile(dungeon: MapData, x: int, y: int) -> void:
+	var tile_position = Vector2i(x, y)
+	var tile: Tile = dungeon.get_tile(tile_position)
+	tile.set_tile_type(dungeon.tile_types.floor)
+
+# Given a rect and the mapdata, carve out a rectangular room.
+func _carve_room(dungeon: MapData, room: Rect2i) -> void:
+	var inner: Rect2i = room.grow(-1)
+	for y in range(inner.position.y, inner.end.y +1):
+		for x in range(inner.position.x, inner.end.x + 1):
+			_carve_tile(dungeon, x, y)
+
+# actually generate a dungeon, right now just carves 2 rooms.
+func generate_dungeon() -> MapData:
+	var dungeon := MapData.new(map_width, map_height)
+	
+	var room_1 := Rect2i(20, 15, 10, 15)
+	var room_2 := Rect2i(35, 15, 10, 15)
+	
+	_carve_room(dungeon, room_1)
+	_carve_room(dungeon, room_2)
+	
+	return dungeon
